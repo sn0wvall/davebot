f1b96a29cfbbca0eaeb1140a983421183a7d9af3
sn0wvall <southernquack@protonmail.com>
2021-01-09 14:33:02 +0000

    Notification Command for Debugging purposes

---
+++ cmds/notiftest.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+
+module.exports.run = async (bot, message, args) => {
+
+	message.author.send("Notification Test")
+
+        return;
+}
+
+module.exports.help = {
+    name: "notification"
+}
